8b6d1cfbd3afc2337d6f521adbafb4873c389a27
Marlon Vélez <marlon.velez.est@tecazuat.edu.ec>
2022-01-16 14:47:02 -0500

    Conexion de ventana Login

--- app/src/main/java/com/example/alaja_music/Login.java
+++ app/src/main/java/com/example/alaja_music/Login.java
@@ -2,12 +2,20 @@ package com.example.alaja_music;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class Login extends AppCompatActivity {
 
+
+    EditText txt_contraseña= (EditText)findViewById(R.id.txt_contraseña);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void ver_password(){
+
+
     }
 }
--- app/src/main/java/com/example/alaja_music/MainActivity.java
+++ app/src/main/java/com/example/alaja_music/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.alaja_music;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void login(View view){
+        Intent window_login = new Intent(this, Login.class);
+        startActivity(window_login);
+    }
+
 }
