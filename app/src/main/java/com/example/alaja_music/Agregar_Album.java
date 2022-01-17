51d26974d031465681bd24bd704bf728822e89a0
WILLIAM <89932700+williamtocto@users.noreply.github.com>
2022-01-16 23:00:50 -0500

    Unir ventanas paso 3

--- app/src/main/java/com/example/alaja_music/Agregar_Album.java
+++ app/src/main/java/com/example/alaja_music/Agregar_Album.java
@@ -1,7 +1,9 @@
 package com.example.alaja_music;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Agregar_Album extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class Agregar_Album extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_agregar_album);
+    }
+
+
+    public void playListV(View playList){
+        Intent window_playList= new Intent(this, PlayList.class);
+        startActivity(window_playList);
     }
 }
--- app/src/main/java/com/example/alaja_music/MainActivity.java
+++ app/src/main/java/com/example/alaja_music/MainActivity.java
@@ -11,8 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
     }
+
+
     public void login(View view){
         Intent window_login = new Intent(this, Login.class);
         startActivity(window_login);
